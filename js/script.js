d9be09bcc7a7603ba02f430d83f4d7f85a1a155c
Weronika Skaczkowska <w.skaczkowska@gmail.com>
2017-12-29 10:39:45 +0100

    Fix validation errors

--- js/script.js
+++ js/script.js
@@ -13,11 +13,11 @@ $(function() {
 		} else {
 			$('nav').removeClass('sticky');
 		}
-	};
+	}
 
 	$('#designControls').on('slid.bs.carousel', function() {
 		var currentSlideName = $('#design-id');
 		var activeSlideNumber = $('div.active').data('number');
 		currentSlideName.text(activeSlideNumber);
 	});
-})
+});
